adbbf4511ca0aea2c48295dca61ccfc38113227a
coderyz <yinzhao@live.cn>
2021-04-09 10:27:28 +0800

    done Promise.then

--- promiseTest.js
+++ promiseTest.js
@@ -1,71 +1,62 @@
-(function (w) {
+(function(w){
   const PENDING = 'pending'
   const RESOLVED = 'resolved'
   const REJECTED = 'rejected'
-
-  function Promise(executor) {
+  function Promise(executor){
     const _this = this
     _this.data = undefined
     _this.status = PENDING
     _this.callbacks = []
-
-    function resolved(value) {
+    function resolve(value){
+      _this.status = RESOLVED
       _this.data = value
       _this.callbacks.forEach(cbObj=>{
         cbObj.onResolved(value)
       })
     }
-
-    function reject(reason) {
+    function reject(reason){
+      _this.status = REJECTED
       _this.data = reason
       _this.callbacks.forEach(cbObj=>{
-        cbObj.onRejected(reason)
+        cbObj.onResolved(reason)
       })
     }
-
-    executor(resolved, reject)
-  }
-
-  Promise.prototype.then = function (onResolved, onRejected) {
-    const _this = this
-    return new Promise((resolve, reject) => {
-      if (_this.status === RESOLVED) {
-        //执行成功的回调
-        setTimeout(() => {
-          try {
-            const result = onResolved(_this.data)
-            if (result instanceof Promise) {
-              //onResolved返回的是Promise
-              result.then(resolve, reject)
-            } else {
+    executor(resolve,reject)
+    Promise.prototype.then = function(onResolved,onRejected){
+      const _this = this
+      return new Promise((resolve,reject) => {
+        function handle(callback){
+          try{
+            const result = callback(_this.data)
+            if (result instanceof Promise){
+              result.then(resolve,reject)
+            }else{
               resolve(result)
             }
-          } catch (error) {
+          }catch(error){
             reject(error)
           }
-        })
-      } else if (_this.status === REJECTED) {
-        //执行失败的回调
-        setTimeout(() => {
-          try {
-            const result = onRejected(_this.data)
-            if (result instanceof Promise) {
-              //onResolved返回的是Promise
-              result.then(resolve, reject)
-            } else {
-              resolve(result)
+        }
+        if (_this.status === RESOLVED){
+          setTimeout(() => {
+            handle(onResolved)
+          })
+        }else if (_this.status === REJECTED){
+          setTimeout(() => {
+            handle(onRejected)
+          })
+        }else{
+          _this.callbacks.push({
+            onResolved(){
+              handle(onResolved)
+            },
+            onRejected(){
+              handle(onRejected)
             }
-          } catch (error) {
-            reject(error)
-          }
-        })
-      } else {
-        _this.callbacks.push({
-          onResolved,
-          onRejected
-        })
-      }
-    })
+          })
+        }
+      })
+    }
   }
   w.Promise = Promise
 })(window)
